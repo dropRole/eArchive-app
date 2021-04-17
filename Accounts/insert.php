2a44c72bade3d3af74b38fb7fa45de7d82f48bf9
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-04-17 17:50:22 +0200

    Script for student account insertion.

---
+++ Accounts/insert.php
@@ -0,0 +1,24 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Accounts/Accounts.php';
+
+// proceed with the session
+session_start();
+
+$id_attendances = $_POST['id_attendances'];
+$pass = $_POST['pass'];
+
+// if id of attendance and password are prosperously passed
+if (isset($id_attendances, $pass)) {
+    // establish a new database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // attempt an account insertion
+    echo $DBC->insertAccount($id_attendances, $pass);
+} // if
