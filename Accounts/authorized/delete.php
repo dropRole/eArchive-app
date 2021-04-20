581ad69f48277d7492ae6174358a6201785cb841
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-04-20 16:32:47 +0200

    Script for account deletion

---
+++ Accounts/authorized/delete.php
@@ -0,0 +1,23 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../../DBC/DBC.php';
+require_once '../../Accounts/Accounts.php';
+
+// proceed with the session
+session_start();
+
+$id_attendances = $_GET['id_attendances'];
+
+// if id of attendance is prosperously passed
+if (isset($id_attendances)) {
+    // establish a new database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // attempt an account deletion
+    echo $DBC->deleteAccount($id_attendances);
+} // if
