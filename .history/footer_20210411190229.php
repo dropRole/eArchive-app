29be06beb39f695e2d136d76cd5d1ccc07e8b8ef
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-04-21 17:17:54 +0200

    Remove code history.

--- .history/footer_20210411190229.php
+++
@@ -1,4 +0,0 @@
-<!-- Bootstrap core JavaScript -->
-<script src="/eArchive/vendor/jquery/jquery.slim.min.js"></script>
-<script src="/eArchive/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-</body>
