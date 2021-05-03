954803d3f024c05b6b088cc8861ea1d082decc0c
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-03 21:07:46 +0200

    Update select.php
    Printing documents source as a data-* attribute value.

--- ScientificPapers/select.php
+++ ScientificPapers/select.php
@@ -41,7 +41,7 @@ if (isset($_GET['id_attendances'])) {
                         if (count($documents))
                             foreach ($documents as $document) {
                         ?>
-                            <li class="list-group-item"><a href="#"><?php echo basename($document->getSource()); ?></a><span class="doc-del-spn ml-3" data-id="<?php echo $document->getIdDocuments(); ?>">&#10007;</span></li>
+                            <li class="list-group-item"><a href="#"><?php echo basename($document->getSource()); ?></a><span class="doc-del-spn ml-3" data-source="<?php echo $document->getSource(); ?>">&#10007;</span></li>
                         <?php
                             } // foreach
                         // if there's no evidence of the documentation
