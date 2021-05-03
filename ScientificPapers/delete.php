ecf13ba3ad217977cae63a877e8279862c8acdf9
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-03 21:05:23 +0200

    Create delete.php
    Script that will be asynchronously executed for deletion of the given scientific paper and its documents.

---
+++ ScientificPapers/delete.php
@@ -0,0 +1,24 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Documents/Documents.php';
+require_once './ScientificPapers.php';
+
+// proceed with the session
+session_start();
+
+$id_scientific_papers = $_GET['id_scientific_papers'];
+
+// if id of scientific paper was prosperously passed
+if (isset($id_scientific_papers)) {
+    // establish a new database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // try a scientific papers deletion with belonging documentation
+    echo $DBC->deleteScientificPaper($id_scientific_papers);
+} // if
