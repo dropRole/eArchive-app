11302356f9b4b22693e1ee43ff99568cbc6891ba
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-05 16:22:24 +0200

    Create upload.php
    Script that will be asynchronously executed for subsequent scientific paper documents upload.

---
+++ Documents/upload.php
@@ -0,0 +1,26 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Documents/Documents.php';
+
+// proceed with the session
+session_start();
+
+$id_scientific_papers = $_POST['id_scientific_papers'];
+$documents = $_POST['documents'];
+
+// if scientific paper id and documents data are successfully passed
+if (isset($id_scientific_papers, $documents)) {
+    // establish a new database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // upload each document
+    foreach($documents as $document){
+        echo $DBC->insertDocument($id_scientific_papers, $document['version'], $document['name']);
+    } // foreach
+} // if
