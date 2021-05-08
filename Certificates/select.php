3e1539ff15cf13650d0b442cdcfcf64ea4d92029
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-08 17:44:35 +0200

    Create select.php
    Script that will be asynchronously executed for exposing certificate particulars.

---
+++ Certificates/select.php
@@ -0,0 +1,35 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Certificates/Certificates.php';
+
+// proceed with the session
+session_start();
+
+$id_attendances = $_GET['id_attendances'];
+
+// if an id of attendances was fowarded by URL query string 
+if (isset($id_attendances)) {
+    // establish a new database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // try to select certificate particulars
+    $certificate = $DBC->selectCertificate($id_attendances);
+    if ($certificate !== NULL) {
+?>
+        <div class="card col-12">
+            <div class="card-body">
+                <h5 class="card-title">Certifikat</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Izdan: <?php echo $certificate->getIssued(); ?></h6>
+                <p class="card-text"><a href="<?php echo "{$certificate->getSource()}"; ?>" target="_blank"><?php echo basename($certificate->getSource()); ?></a></p>
+                <a href="#" class="card-link">Izbriši</a>
+            </div>
+        </div>
+<?php
+    } // if
+} // if
