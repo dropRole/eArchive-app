55db5c1abed0bf827456ca6ea88bca7c55a6b28e
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-08 15:18:56 +0200

    Create insert.php
    Script that will be asynchronously executed for subsequent scientific paper certificate upload/insertion.

---
+++ Graduations/insert.php
@@ -0,0 +1,26 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Certificates/Certificates.php';
+
+// proceed with the session
+session_start();
+
+$id_attendances = $_POST['id_attendances'];
+$certificate = $_POST['certificate'];
+$issued = $_POST['issued'];
+$defended = $_POST['defended'];
+
+// if id of an attendance and certificate are prosperously passed
+if (isset($id_attendances, $certificate, $issued, $defended)) {
+    // establish a new database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // attempt an insertion of graduation data 
+    echo $DBC->insertGraduation($id_attendances, $certificate, (new DateTime($issued)), (new DateTime($defended)));
+} // if
