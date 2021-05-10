22695642161d6c97c9b8b710a2b041c22fcb894a
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-10 18:50:12 +0200

    Create delete.php
    Script that will be asynchronously executed for deletion of the given graduation and certificate particulars.

---
+++ Graduations/delete.php
@@ -0,0 +1,24 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Graduations/Graduations.php';
+
+// proceed with the session
+session_start();
+
+$id_attendances = $_GET['id_attendances'];
+$source = $_GET['source'];
+
+// if ids of an attendance and certificate were forwarded by URL query string 
+if (isset($id_attendances, $source)) {
+    // establish a new database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // try to select certificate particulars
+    echo $DBC->deleteGraduation($id_attendances, $source);
+} // if
