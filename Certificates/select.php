8d4521f196735b469f905a946ece5a47c868d8d1
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-10 18:42:14 +0200

    Update select.php
    Altered URL of hypertext reference attribute and added data-* attributes with proper id values to graduation certificate deletion anchor.

--- Certificates/select.php
+++ Certificates/select.php
@@ -14,20 +14,20 @@ session_start();
 
 $id_attendances = $_GET['id_attendances'];
 
-// if an id of attendances was fowarded by URL query string 
+// if an id of attendance was forwarded by URL query string 
 if (isset($id_attendances)) {
     // establish a new database connection
     $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
     // try to select certificate particulars
-    $certificate = $DBC->selectCertificate($id_attendances);
+    $certificate = $DBC->selectCertificate($id_attendances)[0];
     if ($certificate !== NULL) {
 ?>
         <div class="card col-12">
             <div class="card-body">
                 <h5 class="card-title">Certifikat</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Izdan: <?php echo $certificate->getIssued(); ?></h6>
-                <p class="card-text"><a href="<?php echo "{$certificate->getSource()}"; ?>" target="_blank"><?php echo basename($certificate->getSource()); ?></a></p>
-                <a href="#" class="card-link">Izbriši</a>
+                <p class="card-text"><a href="<?php echo "../../{$certificate->getSource()}"; ?>" target="_blank"><?php echo basename($certificate->getSource()); ?></a></p>
+                <a href="#" class="card-link cert-del-a" data-att-id="<?php echo $id_attendances; ?>" data-source="<?php echo $certificate->getSource(); ?>">Izbriši</a>
             </div>
         </div>
 <?php
