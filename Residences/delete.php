126fa27cc99213634f9452353d0a637f348b2e86
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-18 17:32:28 +0200

    Create delete.php
    Script that should be asynchronously run for deletion of student temporal or permanent residence data.

---
+++ Residences/delete.php
@@ -0,0 +1,24 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once './Residences.php';
+
+// proceed with the current session
+session_start();
+
+$id_students = $_GET['id_students'];
+$id_residences = $_GET['id_residences'];
+
+// if id of a student and his residence were prosperously passed 
+if (isset($id_students, $id_residences)) {
+    // drive a new connection with the database server 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // attempt student residence deletion 
+    echo $DBC->deleteStudentResidence($id_students, $id_residences);
+} // if
