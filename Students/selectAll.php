e9149f4d385a21eec03dfba2ca6f88db4e92529a
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-18 17:33:57 +0200

    Create selectAll.php
    Script that should be asynchronously run to update the student evidence tally.

---
+++ Students/selectAll.php
@@ -0,0 +1,88 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Students/Students.php';
+require_once '../Certificates/Certificates.php';
+
+// proceed with the session
+session_start();
+// establish a new database connection
+$DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+?>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Ime in priimek</th>
+            <th>Indeks</th>
+            <th>Program</th>
+            <th>Stopnja programa</th>
+            <th>Fakulteta</th>
+            <th>Znanstvena dela</th>
+            <th>Certifikat</th>
+            <th colspan="2">Račun</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        // select all students records
+        $students = $DBC->selectStudents();
+        // for each student in the record
+        foreach ($students as $student) {
+        ?>
+            <tr>
+                <td><?php echo $student->fullname; ?></td>
+                <td><?php echo $student->index; ?></td>
+                <td><?php echo $student->program; ?></td>
+                <td><?php echo $student->degree; ?></td>
+                <td><?php echo $student->faculty; ?></td>
+                <td>
+                    <a class="sp-vw-a" href="#sPVMdl" data-toggle="modal" data-id="<?php echo $student->id_attendances; ?>">Pregled</a>
+                    <a class="sp-ins-a" href="#sPIUMdl" data-toggle="modal" data-id="<?php echo $student->id_attendances; ?>">Vstavljanje</a>
+                </td>
+                <td>
+                    <?php
+                    // if student possesses a certificate
+                    if ($DBC->selectCertificate($student->id_attendances) != NULL) {
+                    ?>
+                        <a class="cert-vw-a" href="#certMdl" data-toggle="modal" data-id="<?php echo $student->id_attendances; ?>">Pregled</a>
+                    <?php
+                    } // if
+                    // if student doesn't  possess a certificate
+                    if ($DBC->selectCertificate($student->id_attendances) == NULL) {
+                    ?>
+                        <a class="cert-ins-a" href="#certIUMdl" data-toggle="modal" data-id="<?php echo $student->id_attendances; ?>">Vstavljanje</a>
+                    <?php
+                    } // if
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    // if assigned an account 
+                    if ($DBC->checkStudentAccount($student->id_attendances)) {
+                    ?>
+                        Dodeljen: <span class="text-warning"><?php echo $DBC->getAccountParticulars($student->id_attendances); ?></span>
+                        <span class="acc-del-btn" data-id="<?php echo $student->id_attendances; ?>">&#10007;</span>
+                    <?php
+                    } // if
+                    else {
+                    ?>
+                        <button class="btn btn-warning acc-ins-btn" type="button" value="<?php echo $student->id_attendances; ?>" data-toggle="modal" data-target="#aMdl">Ustvari</button>
+                    <?php
+                    } // else
+                    ?>
+                </td>
+                <td>
+                    <a class="stu-upd-a" href="#" data-id="<?php echo $student->id_students; ?>">Uredi</a>
+                </td>
+            </tr>
+        <?php
+        } // foreach
+        ?>
+    </tbody>
+</table>
