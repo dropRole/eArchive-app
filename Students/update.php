3d6b15c4e9ee85a8ba35df5d9dfa77ef1e5361c3
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-18 17:34:53 +0200

    Create update.php
    Script that should be asynchronously executed to update data of a student.

---
+++ Students/update.php
@@ -0,0 +1,29 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once './Students.php';
+
+session_start();
+
+$id_students = $_POST['id_students'];
+$id_postal_codes = $_POST['id_postal_codes'];
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$residences = $_POST['residences'];
+// if student particulars were prosperously passed
+if (isset($id_students, $id_postal_codes, $name, $surname, $residences)) {
+    // establish a new connection with the database server
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // report on update
+    echo $DBC->updateStudent($id_students, $id_postal_codes, $name, $surname, $email, $telephone, $residences);
+} // if 
+
+?>
