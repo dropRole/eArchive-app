6636e2915d0fdba7421af5bfe62a69b09c74fc35
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-05-20 16:55:58 +0200

    Create delete.php
    Script that should be asynchronously executed to delete all the data related to a particular student.

---
+++ Students/delete.php
@@ -0,0 +1,30 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Residences/Residences.php';
+require_once '../Graduations/Graduations.php';
+require_once '../Certificates/Certificates.php';
+require_once '../Documents/Documents.php';
+require_once '../ScientificPapers/ScientificPapers.php';
+require_once '../Attendances/Attendances.php';
+require_once './Students.php';
+
+// proceed with the current session
+session_start();
+
+$id_students = $_GET['id_students'];
+$id_attendances = $_GET['id_attendances'];
+
+// if id of a student and its program attendance id were forwarded by URL query string
+if (isset($id_students, $id_attendances)) {
+    // create a new PDO interface object instance 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // attemt a deletion
+    echo $DBC->deleteStudent($id_students, $id_attendances);
+} // if 
