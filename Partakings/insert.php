cf731de36deb3cd81d7464905d155a7090816ca2
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-05 15:33:48 +0200

    Create insert.php
    Script that should be asynchronously run to insert data of scientific paper mentor.

---
+++ Partakings/insert.php
@@ -0,0 +1,31 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Partakings/Partakings.php';
+
+// proceed with the session
+session_start();
+
+$id_scientific_papers = $_POST['id_scientific_papers'];
+$partakers = $_POST['partakers'];
+
+// if id of a scientific paper and partakers were successfully submitted
+if (isset($id_scientific_papers, $partakers)) {
+    // establish a new database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // insert and upload each document
+    foreach ($partakers as $partaker)
+        // if partaker was determined
+        if ($DBC->insertPartakerOnScientificPaper($id_scientific_papers, $DBC->selectStudentsByIndex($partaker['index'])[0]->id_attendances, $partaker['part']))
+            echo "Soavtor {$DBC->selectStudentsByIndex($partaker['index'])[0]->fullname} je uspešno dodeljen.";
+        else
+            echo "Soavtor {$DBC->selectStudentsByIndex($partaker['index'])[0]->fullname} ni uspešno dodeljen.";
+} // if
+else
+    echo 'Napaka: potrebni podatki niso posredovani.';
