c409380f4f8c037df8d92303776e325f607a62fb
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-07 18:30:53 +0200

    Create delete.php
    Script that should be asynchronously executed to delete data referenced to mentoring of the scientific paper.

---
+++ Mentorings/delete.php
@@ -0,0 +1,21 @@
+<?php
+
+// namespace and class import declaration
+use DBC\DBC;
+
+// script import declaration
+require_once '../DBC/DBC.php';
+require_once '../Mentorings/Mentorings.php';
+
+// proceed with the session
+session_start();
+
+$id_mentorings = $_GET['id_mentorings'];
+
+// if record id was passed by URL query string
+if (isset($id_mentorings)) {
+    // return a new PDO object instance that carries connection with the database server 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // try deletion 
+    echo $DBC->deleteMentorOfScientificPaper($id_mentorings);
+} // if
