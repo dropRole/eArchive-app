f267f3c87ae7512e7a1ece33b845985ab2f0caa0
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-07 18:03:56 +0200

    Create insert.php
    Script that should be asynchronously executed to interpolate data of scientific paper mentor.

---
+++ Mentorings/insert.php
@@ -0,0 +1,27 @@
+<?php
+
+// namespace and class import declaration
+use DBC\DBC;
+
+// script import declaration
+require_once '../DBC/DBC.php';
+require_once '../Mentorings/Mentorings.php';
+
+// proceed with the session
+session_start();
+
+$id_scientific_papers = $_POST['id_scientific_papers'];
+$mentors = $_POST['mentors'];
+
+// if id of scientific paper and array of mentors data were prosperously committed
+if (isset($id_scientific_papers, $mentors)) {
+    // return a new PDO object instance that carries connection with the database server 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // insert data of each mentor
+    foreach ($mentors as $mentor)
+        // if data was successfully inserted 
+        if ($DBC->insertMentorOfScientificPaper($id_scientific_papers, $mentor['id_faculties'], $mentor['mentor'], $mentor['taught'], $mentor['email'], $mentor['telephone']))
+            echo "{$mentor['mentor']} je uspešno določen";
+        else
+            echo "{$mentor['mentor']} ni uspešno določen";
+} // if
