62df7a3d00573f53e6271157ea6c02ae7af9c17d
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-07 10:37:38 +0200

    Create update.php
    Script that should be asynchronously run to update data of scientific paper partaker.

---
+++ Partakings/update.php
@@ -0,0 +1,22 @@
+<?php
+
+// namespace and class import declaration
+use DBC\DBC;
+
+// script import declaration
+require_once '../DBC/DBC.php';
+require_once '../Partakings/Partakings.php';
+
+// proceed with the session
+session_start();
+
+$id_partakings = $_POST['id_partakings'];
+$partaker = $_POST['partakers'];
+
+// if id and part of the partaking were successfully submitted
+if (isset($id_partakings, $partaker)) {
+    // return a new PDO object instance that carries connection with the database server 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // update the part
+    echo $DBC->updatePartInScientificPaper($id_partakings, $partaker[1]['part']);
+} // if
