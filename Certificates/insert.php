dcecf22be95cf7f8bc0925a6079257a5999f1ddb
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-11 16:10:10 +0200

    Create insert.php
    Script that should be asynchronously run  to upload graduation certificate and data concerning the graduation of a student attending a particular program.

---
+++ Certificates/insert.php
@@ -0,0 +1,28 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Certificates/Certificates.php';
+
+// proceed with the session
+session_start();
+
+$id_attendances = $_POST['id_attendances'];
+$certificate = $_POST['certificate'];
+$defended = $_POST['defended'];
+$issued = $_POST['issued'];
+
+// if graduation data was prospeoursly forwarded  
+if (isset($id_attendances, $certificate, $defended, $issued)) {
+    // return a new PDO object instance that carries connection with the database server 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // if certificate was uploaded
+    if ($certificate !== NULL)
+        // attempt a graduation certificate insertion
+        echo $DBC->insertGraduation($id_attendances, $certificate, (new DateTime($defended)), (new DateTime($issued)));
+} // if
