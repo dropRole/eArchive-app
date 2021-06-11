601a52bf2cae27947d79c53e608fbfe005c3612e
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-11 14:24:37 +0200

    Update insert.php

--- ScientificPapers/insert.php
+++ ScientificPapers/insert.php
@@ -31,7 +31,7 @@ if (isset($id_attendances, $topic, $type, $written, $documents)) {
         if (isset($_POST['partakers']))
             foreach ($_POST['partakers'] as $partaker)
                 // if partaker was succesfully inserted 
-                if ($DBC->insertPartakingsOnScientificPaper($report['id_scientific_papers'], $DBC->selectStudentsByIndex($partaker['index'])[0]->id_attendances, $partaker['part']))
+                if ($DBC->insertPartakerOnScientificPaper($report['id_scientific_papers'], $DBC->selectStudentsByIndex($partaker['index'])[0]->id_attendances, $partaker['part']))
                     echo "Soavtor {$DBC->selectStudentsByIndex($partaker['index'])[0]->fullname} je uspešno vstavljen." . PHP_EOL;
                 else
                     echo "Soavtor {$DBC->selectStudentsByIndex($partaker['index'])[0]->fullname} ni uspešno vstavljen." . PHP_EOL;
