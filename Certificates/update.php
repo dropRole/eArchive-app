70f72f0044ea5c3d1a7d9dc9ef318140e9b58d35
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-13 14:49:40 +0200

    Create update.php
    Script that should be asynchronously executed to update the date of defence and issuance of the graduation certificate.

---
+++ Certificates/update.php
@@ -0,0 +1,27 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once '../Certificates/Certificates.php';
+
+// proceed with the session
+session_start();
+
+$id_certificates = $_POST['id_certificates'];
+$defended = $_POST['defended'];
+$issued = $_POST['issued'];
+
+// if graduation certificate data to be updated was successfully submitted
+if (isset($id_certificates, $defended, $issued)) {
+    // return a new PDO object instance that carries connection with the database server 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // update date of issuing 
+    echo $DBC->updateGradCertIssuingDate($id_certificates, (new DateTime($issued))) . PHP_EOL;
+    // update date of defenense
+    echo $DBC->updateGraduationDefenseDate($id_certificates, (new DateTime($defended)));
+} // if
