75a0f6d019dac1b2f9940e6bbfbb8aabdcc74c09
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-16 16:22:56 +0200

    Update update.php
    Altered reference to the class method.

--- Certificates/update.php
+++ Certificates/update.php
@@ -23,5 +23,5 @@ if (isset($id_certificates, $defended, $issued)) {
     // update date of issuing 
     echo $DBC->updateGradCertIssuingDate($id_certificates, (new DateTime($issued))) . PHP_EOL;
     // update date of defenense
-    echo $DBC->updateGraduationDefenseDate($id_certificates, (new DateTime($defended)));
+    echo $DBC->updateGraduationDefenceDate($id_certificates, (new DateTime($defended)));
 } // if
