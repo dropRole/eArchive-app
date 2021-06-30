0c5d36f0f7fc69c8d3015487861940fc16cb2012
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-06-30 15:57:50 +0200

    Update login.php
    Altered global id attribute values of elements. Interpolated input elements inside corresponding labels.

--- login.php
+++ login.php
@@ -8,18 +8,20 @@ include_once './header.php';
 
 <section id="login">
     <div class="d-flex justify-content-center">
-        <form id="lFrm" method="POST" action="./Accounts/authentication.php">
+        <form id="lgnFrm" method="POST" action="./Accounts/authentication.php">
             <p class="h1 mb-3">Prijava</p>
             <div class="form-group">
-                <label for="iInpt">Indeks</label>
-                <input id="iInpt" class="form-control" type="text" name="index" autocomplete="on" required>
+                <label>Indeks
+                    <input id="indexInptEl" class="form-control" type="text" name="index" autocomplete="on" required>
+                </label>
             </div>
             <div class="form-group">
-                <label for="pInpt">Geslo</label>
-                <input id="pInpt" class="form-control" type="password" name="pass" autocomplete="current-password" required>
+                <label>Geslo
+                    <input id="pаssInptEl" class="form-control" type="password" name="pass" autocomplete="current-password" required>
+                </label>
             </div>
-            <div id="lRprt"></div>
-            <input id="lBtn" class="btn btn-dark" type="submit" value="Potrdi">
+            <div id="lgnRprt"></div>
+            <input id="lgnBtn" class="btn btn-dark" type="submit" value="Potrdi">
         </form>
     </div>
 </section>
