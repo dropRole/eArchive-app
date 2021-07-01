2a6bd5486c278b9736ef5d7c14668b8d7ab77899
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-01 15:34:18 +0200

    Update home.php
    Importing DBC namespace and class through required DBC.php script and instantiating an object for establishing connection with the database server. Declared header.php and footer.php script import.

---
+++ Accounts/student/home.php
@@ -0,0 +1,24 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../../DBC/DBC.php';
+include_once '../../header.php';
+
+$DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+
+?>
+
+
+
+<?php
+
+// script import declaration
+
+include_once '../../footer.php';
+
+?>
