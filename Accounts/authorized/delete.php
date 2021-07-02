a24203376bce2010b8d3e3d80a54d4f324c6bf2b
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-02 16:20:42 +0200

    Getting index number throughout URL query string
    Passing $index variable as second argument of deleteStudtAcct function.

--- Accounts/authorized/delete.php
+++ Accounts/authorized/delete.php
@@ -13,11 +13,12 @@ require_once '../../Accounts/Accounts.php';
 session_start();
 
 $id_attendances = $_GET['id_attendances'];
+$index = $_GET['index'];
 
 // if id of attendance is prosperously passed
-if (isset($id_attendances)) {
+if (isset($id_attendances, $index)) {
     // establish a new database connection
     $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
     // attempt an account deletion
-    echo $DBC->deleteStudentAccount($id_attendances);
+    echo $DBC->deleteStudtAcct($id_attendances, $index);
 } // if
