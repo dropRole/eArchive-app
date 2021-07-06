65bafba5ea5736f3531a3fa0084f4e88737f4430
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-06 16:56:16 +0200

    Defining footer element
    Interpolated division element comprised of credits and hyperlink to the author of the custom made user icon used as a default user avatar.

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
+<foooter>
+    <div>Icons made by
+        <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </div>
+</foooter>
 <!-- Bootstrap core JavaScript -->
 <script src="/eArchive/vendor/jquery/jquery.slim.min.js"></script>
 <script src="/eArchive/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
