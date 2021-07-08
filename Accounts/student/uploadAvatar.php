7747c6da7a09d72ebf874642a2f2ee6030f231f8
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-08 16:29:20 +0200

    Create uploadAvatar.php
    Asynchronously executed script for uploading account avatar of the logged in student.

---
+++ Accounts/student/uploadAvatar.php
@@ -0,0 +1,23 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../../DBC/DBC.php';
+require_once '../Accounts.php';
+
+// proceed with the session
+session_start();
+
+$id_attendances = $_POST['id_attendances'];
+
+// if id of program attendance was prosperously submitted
+if (isset($id_attendances)) {
+    // return a new PDO object instance that carries connection with the database server 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // attempt avatar upload
+    echo $DBC->uploadAcctAvtr($id_attendances);
+} // if
