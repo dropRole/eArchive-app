aead4c0b65c02ef068f51bce541afe69c024c83f
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-08 16:25:56 +0200

    Structure of the navigation bar for authorized user

---
+++ Accounts/authorized/nav.php
@@ -0,0 +1,31 @@
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">eArhiv</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Domov
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="studentRecord.php">Študenti
+                        <span class="sr-only">Študenti</span>
+                    </a>
+                </li>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-outline-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="/eArchive/custom/img/user.png">
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="../logout.php">Odjava</a>
+                    </div>
+                </div>
+            </ul>
+        </div>
+    </div>
+</nav>
