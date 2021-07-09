b4912732388fa6e20941bc81cf17712859237d32
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-09 17:15:58 +0200

    Create deleteAvatar.php
    Asynchronously script run for conducting of deleting an account avatar of the logged student.

---
+++ Accounts/student/deleteAvatar.php
@@ -0,0 +1,24 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../../DBC/DBC.php';
+require_once '../Accounts.php';
+
+// proceed with the session
+session_start();
+
+$id_attendances = $_GET['id_attendances'];
+$avatar = $_GET['avatar'];
+
+// if id of program attendance and location of an avatar were prosperously passed via URL query string
+if (isset($id_attendances, $avatar)) {
+    // return a new PDO object instance that carries connection with the database server 
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // attempt avatar deletion
+    echo $DBC->deleteAcctAvatar($id_attendances, $avatar);
+} // if
