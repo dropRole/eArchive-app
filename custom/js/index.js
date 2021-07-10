215eed39647f9ef1d72149153a098bebcb2403d6
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-10 15:28:42 +0200

    Update index.js
    Latterly declared globally scoped drpDwnItms variable and request arrow function expression.

--- custom/js/index.js
+++ custom/js/index.js
@@ -1,6 +1,38 @@
 // IIFE
 (() => {
     // globally encompassed variable declaration
-    var searchInptEl = document.getElementById('searchInptEl') // input element for search for scientific papers
+    var searchInptEl = document.getElementById('searchInptEl'), // input element for search for scientific papers
+        drpDwnItms = document.getElementsByClassName('dropdown-item') // dropdown items for denoting type of search
+
+    /*
+     *   instantiate an object of integrated XHR interface and make an asynchronous operation on a script   
+     *   @param String script
+     *   @param String method
+     *   @param String responseType 
+     *   @param FormData frmData 
+     */
+    let request = (script, method, resType = '', frmData = null) => {
+            return new Promise((resolve, reject) => {
+                // instanitate an XHR object
+                let xmlhttp = new XMLHttpRequest()
+                xmlhttp.addEventListener(
+                        'load',
+                        () => {
+                            // resolve the promise if transaction was successful
+                            resolve(xmlhttp.response)
+                        }
+                    ) // addEventListener
+                xmlhttp.addEventListener(
+                        'error',
+                        () => {
+                            // reject the promise if transaction encountered an error
+                            reject('Prišlo je do napake na strežniku!')
+                        }
+                    ) // addEventListener
+                xmlhttp.open(method, script, true)
+                xmlhttp.responseType = resType
+                xmlhttp.send(frmData)
+            })
+        } // request
 
 })()
