d69a52d1082f7483ed35f1f0f03bb7e0fa401ae3
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-10 15:21:28 +0200

    Update index.js
    Implemented IIFE design pattern. Defined globally encompassed searchInptEl variable storing input element for searching among archived scientific papers.

---
+++ custom/js/index.js
@@ -0,0 +1,6 @@
+// IIFE
+(() => {
+    // globally encompassed variable declaration
+    var searchInptEl = document.getElementById('searchInptEl') // input element for search for scientific papers
+
+})()
