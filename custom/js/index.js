c87bf9a6ce2a1c8aeabea2d20cde7b95fe42477f
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-10 16:03:15 +0200

    Update index.js
    Newly declared globally encompassed frag variable holding an instance of DocumentFragment object. Attached input event listener to searchInptEl. Attached click event listener to each of  drpDwnItms collection elements.

--- custom/js/index.js
+++ custom/js/index.js
@@ -1,8 +1,34 @@
 // IIFE
 (() => {
     // globally encompassed variable declaration
-    var searchInptEl = document.getElementById('searchInptEl'), // input element for search for scientific papers
+    var frag = new DocumentFragment(), // minimal passive document object 
+        searchInptEl = document.getElementById('searchInptEl'), // input element for search for scientific papers
         drpDwnItms = document.getElementsByClassName('dropdown-item') // dropdown items for denoting type of search
+
+    searchInptEl.addEventListener(
+            'input',
+            e => request(
+                `/eArchive/ScientificPapers/filter.php?${e.target.dataset.criterion}=${e.target.value}`,
+                'GET',
+                'document'
+            )
+            .then(response => {
+                // comprise a node tree structure
+                frag = response
+                    // replace nodes of the active with the passive document nodes 
+                document.body.querySelector('#sciPapSrchRslt').replaceWith(frag.body.querySelector('#sciPapSrchRslt'))
+            })
+        ) // addEventListener
+
+    Array.from(drpDwnItms, item => {
+        item.addEventListener(
+                'click',
+                () => {
+                    searchInptEl.dataset.criterion = item.dataset.criterion
+                    searchInptEl.placeholder = item.dataset.placeholder
+                }
+            ) // addEventListener
+    })
 
     /*
      *   instantiate an object of integrated XHR interface and make an asynchronous operation on a script   
