755adfc2790d31aa2149fff650b58082176cad18
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-10 18:13:11 +0200

    Interpolated modal for scientific paper partakers view

---
+++ modals.php
@@ -0,0 +1,16 @@
+<!-- Modal for scientific paper partakers view -->
+<div class="modal fade" id="sciPapPrtViewMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Soavtroji dela</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+            </div>
+        </div>
+    </div>
+</div>
