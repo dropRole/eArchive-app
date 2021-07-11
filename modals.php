f030a00e424f5703188e450edb112b5deea6271c
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-11 20:02:35 +0200

    Interpolating modal viewing documents of scientific papers

--- modals.php
+++ modals.php
@@ -14,3 +14,19 @@
         </div>
     </div>
 </div>
+<!-- Modal for scientific paper document view -->
+<div class="modal fade" id="sciPapDocsViewMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Dokumenta dela</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+            </div>
+        </div>
+    </div>
+</div>
