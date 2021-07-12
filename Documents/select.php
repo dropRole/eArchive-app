8d752561e0198c916edbb1ba4b9d442423a297d5
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-12 16:03:35 +0200

    Create select.php
    Asynchronously executed script for selecting uploaded documents of the given archived scientific paper.

---
+++ Documents/select.php
@@ -0,0 +1,49 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../DBC/DBC.php';
+require_once './Documents.php';
+
+$id_scientific_papers = $_GET['id_scientific_papers'];
+
+// if id of a scientific paper was successfully passed via URL query string
+if (isset($id_scientific_papers)) {
+    // retrieve an instance of PDO holding database server connection
+    $DBC = new DBC();
+?>
+    <div class="table-responsive">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Objavljen</th>
+                    <th>Verzija</th>
+                    <th>Lokacija</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // select documents of the givne scientific paper
+                foreach ($DBC->selectDocuments($id_scientific_papers) as $doc) {
+                ?>
+                    <tr>
+                        <td><?php echo (new DateTime($doc->getPublished()))->format('d-m-Y'); ?></td>
+                        <td><?php echo $doc->getVersion(); ?></td>
+                        <td>
+                            <a href="<?php echo "/eArchive/{$doc->getSource()}"; ?>" target="_blank"><?php echo basename($doc->getSource()); ?></a>
+                        </td>
+                    </tr>
+                <?php
+                } // foreach
+                ?>
+            </tbody>
+        </table>
+    </div>
+<?php
+} // if
+
+?>
