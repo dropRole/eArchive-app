afeb81d83485e55eb7ffb74a84ff49d66573773f
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-13 17:28:12 +0200

    Update select.php
    Invoked DBC constructor method without passing database server access credentials. Altered card structure by interpolating the diploma defence date. Enabling data manipulation regarding the diploma to merely authorized user.

--- Certificates/select.php
+++ Certificates/select.php
@@ -17,7 +17,7 @@ $id_attendances = $_GET['id_attendances'];
 // if an id of attendance was forwarded by URL query string 
 if (isset($id_attendances)) {
     // establish a new database connection
-    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    $DBC = new DBC();
     // try to select certificate particulars
     $certificate = $DBC->selectCertificate($id_attendances)[0];
     if ($certificate !== NULL) {
@@ -25,12 +25,20 @@ if (isset($id_attendances)) {
         <div class="card col-12">
             <div class="card-body">
                 <h5 class="card-title">Certifikat</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Zagovorjen: <?php echo $certificate->defended; ?></h6>
                 <h6 class="card-subtitle mb-2 text-muted">Izdan: <?php echo $certificate->getIssued(); ?></h6>
                 <p class="card-text">
-                    <a href="<?php echo "../../{$certificate->getSource()}"; ?>" target="_blank"><?php echo basename($certificate->getSource()); ?></a>
+                    <a href="<?php echo "/eArchive/{$certificate->getSource()}"; ?>" target="_blank"><?php echo basename($certificate->getSource()); ?></a>
                 </p>
-                <a href="#gradCertUpldMdl" class="card-link cert-upd-a" data-id-certificates="<?php echo $certificate->getIdCertificates(); ?>" data-defended="<?php echo $certificate->defended; ?>" data-issued="<?php echo $certificate->getIssued(); ?>" data-toggle="modal">Uredi</a>
-                <a href="#" class="card-link cert-del-a" data-id-attendances="<?php echo $id_attendances; ?>" data-source="<?php echo $certificate->getSource(); ?>">Izbriši</a>
+                <?php
+                // if the authorized is logged in
+                if (isset($_SESSION['authorized'])) {
+                ?>
+                    <a href="#gradCertUpldMdl" class="card-link cert-upd-a" data-id-certificates="<?php echo $certificate->getIdCertificates(); ?>" data-defended="<?php echo $certificate->defended; ?>" data-issued="<?php echo $certificate->getIssued(); ?>" data-toggle="modal">Uredi</a>
+                    <a href="#" class="card-link cert-del-a" data-id-attendances="<?php echo $id_attendances; ?>" data-source="<?php echo $certificate->getSource(); ?>">Izbriši</a>
+                <?php
+                } // if
+                ?>
             </div>
         </div>
 <?php
