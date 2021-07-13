68f6c4e5f2d1b9ffcb7d152abd75a819b91593f7
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-13 15:54:04 +0200

    Added modal
    Providing insight into the diploma issued upon scientific paper defence.

--- modals.php
+++ modals.php
@@ -30,3 +30,19 @@
         </div>
     </div>
 </div>
+<!-- Modal for graduation certificate view -->
+<div class="modal fade" id="gradCertViewMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Certifikat</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+            </div>
+        </div>
+    </div>
+</div>
