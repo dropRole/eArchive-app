1632edf4cb42012ae9d300011aa33ada5f98744f
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-13 15:51:49 +0200

    Interpolating the navigation bar

--- login.php
+++ login.php
@@ -6,6 +6,16 @@ include_once './header.php';
 
 ?>
 
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+        <a class="navbar-brand" href="/eArchive/index.php">eArhiv</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+
 <section id="login">
     <div class="d-flex justify-content-center">
         <form id="lgnFrm" method="POST" action="./Accounts/authentication.php">
