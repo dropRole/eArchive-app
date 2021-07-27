b5c2565dd707f1ac064bf4f3740e3fe1fcde3753
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-27 17:54:47 +0200

    Update nav.php
    Prepended an image to an anchor element directing to the initial page.

--- nav.php
+++ nav.php
@@ -1,7 +1,9 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
-        <a class="navbar-brand" href="/eArchive/index.php">eArhiv</a>
+        <a class="navbar-brand" href="/eArchive/index.php">
+            <img src="/eArchive/custom/img/eArchive.png">&nbsp;eArhiv
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
