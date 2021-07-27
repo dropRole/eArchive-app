c9a92396f194acf92f1536624afd3f3fc8827626
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-27 17:51:30 +0200

    Update header.php
    Interpolated location of the favicon.

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ require_once __DIR__ . './session.php';
     <meta name='description' content='eArchive for student sctientific achivements' />
     <title>eArchive</title>
     <!-- Favicon-->
-    <link rel='icon' type='image/x-icon' href='#' />
+    <link rel='icon' type='image/x-icon' href='/eArchive/custom/img/eArchive.png' />
     <!-- Core template CSS (includes Bootstrap) -->
     <link href='/eArchive/vendor/bootstrap/css/bootstrap.css' rel='stylesheet' />
     <!-- Custom CSS design -->
