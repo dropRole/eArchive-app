0ef654dea1e1c605436ceaece0eed1b445012c0e
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-27 13:44:54 +0200

    Referencing renamed method

--- Students/selectAll.php
+++ Students/selectAll.php
@@ -62,7 +62,7 @@ $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
                     // if student is assigned an account to  
                     if ($DBC->checkAcctAssignment($student->id_attendances)) {
                     ?>
-                        Dodeljen: <span class="text-warning"><?php echo $DBC->getAccountParticulars($student->id_attendances); ?></span>
+                        Dodeljen: <span class="text-warning"><?php echo $DBC->selectAcctGrantDate($student->id_attendances); ?></span>
                         <span class="acc-del-btn" data-id-attendances="<?php echo $student->id_attendances; ?>" data-index="<?php echo $student->index; ?>">&#10007;</span>
                     <?php
                     } // if
