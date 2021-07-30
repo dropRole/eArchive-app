4368d7b78d9ba91daf519232d43d5dafa0cb4c67
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-07-30 18:38:56 +0200

    Update footer.php
    Defined classes of the footer and its nested elements.

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
-<foooter>
-    <div>Icons made by
-        <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+<footer class="d-flex justify-content-center align-items-center">
+    <div class="text-white">Icons made by
+        <a class="text-decoration-none text-warning" href="https://www.freepik.com" title="Freepik">Freepik</a>
+        from
+        <a class="text-decoration-none text-warning" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
     </div>
-</foooter>
+</footer>
 <!-- Bootstrap core JavaScript -->
 <script src="/eArchive/vendor/jquery/jquery.slim.min.js"></script>
 <script src="/eArchive/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
