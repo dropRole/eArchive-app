d1b74b4f92d2b40142ae7b8d35bcc089bc74465d
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-06 13:44:06 +0200

    Create select.php
    Script intentionally run in the asynchronous manner selecting permanent and temporary residences of the particular student.

---
+++ Residences/select.php
@@ -0,0 +1,22 @@
+<?php
+
+// namespace and class import declaration
+
+use DBC\DBC;
+
+// script import declaration
+
+require_once '../autoload.php';
+
+// proceed with the current session
+session_start();
+
+$id_students = $_GET['id_students'];
+
+// if id of a student was passed by URL query string
+if (isset($id_students)) {
+    // retrieve a PDO instance holding database connection
+    $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
+    // select student residences
+    echo $DBC->selectStudtResidences($id_students);
+} // if 
