91184a06015be28fd0cd95217e8ff1bb2a144ea1
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-06 14:17:55 +0200

    Update evidence.js
    Enabling the icons tooltips on latterly scientific achievements table load.

--- custom/js/evidence.js
+++ custom/js/evidence.js
@@ -86,6 +86,8 @@
                     frag = response
                         // reflect fragments body  
                     document.querySelector('div.table-responsive').innerHTML = frag.body.innerHTML
+                        // enabling tooltips
+                    $('[data-toggle="tooltip"]').tooltip()
                 })
                 .then(() => listenStudtEvidTbl())
                 .catch(error => alert(error)) // catch
