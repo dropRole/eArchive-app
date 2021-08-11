e2e91555d8f7f9d77e345217d2a8a83b960b6a41
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-11 11:28:48 +0200

    Update login.php
    Disposing of nav snippet and importing nav.php script composed of navigation markup.

--- login.php
+++ login.php
@@ -4,22 +4,14 @@
 
 include_once './header.php';
 
-?>
+include_once './nav.php';
 
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-    <div class="container">
-        <a class="navbar-brand" href="/eArchive/index.php">eArhiv</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    </div>
-</nav>
+?>
 
 <section id="login">
     <div class="d-flex justify-content-center">
-        <form id="lgnFrm" method="POST" action="./Accounts/authentication.php">
-            <p class="h1 mb-3">Prijava</p>
+        <form id="lgnFrm" method="POST" action="./Accounts/authentication.php" class="my-3">
+            <p class="h2 mb-3">Prijava</p>
             <div class="form-group">
                 <label>Indeks
                     <input id="indexInptEl" class="form-control" type="text" name="index" autocomplete="on" required>
