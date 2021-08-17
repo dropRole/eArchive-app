f0148ff0863b3ff7f337a3608ec3c22f41873c0b
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-17 19:28:47 +0200

    Update index.php
    Deferred load of index.js script. Restructuring scientific achievements evidence table.

--- index.php
+++ index.php
@@ -14,13 +14,17 @@ $DBC = new DBC();
 include_once './nav.php';
 
 ?>
+
+<!-- Deferred load of JS script -->
+<script defer src="./custom/js/index.js"></script>
+
 <div class="container my-5">
     <div class="h2 mb-4 p-4 text-center">
         Digitalna arhiva<br>znanstvenih dosežkov
         <div class="heading-border-top-left"></div>
         <div class="heading-border-bottom-right"></div>
     </div>
-    <div class="input-group">
+    <div class="input-group mb-2">
         <input id="searchInptEl" type="text" class="form-control" data-criterion="author" placeholder="Ime in priimek avtorja dela" aria-label="Text input with segmented dropdown button">
         <div class="input-group-append">
             <button type="button" class="btn btn-dark">Pregled</button>
@@ -54,12 +58,12 @@ include_once './nav.php';
                     <tr>
                         <td><?php echo $sciPap->getTopic(); ?></td>
                         <td>
-                            <a class="stu-vw-a" href="#studtViewMdl" data-toggle="modal" data-id-attendances="<?php echo $sciPap->getIdAttendances(); ?>"><?php echo $sciPap->author; ?></a>
+                            <a class="stu-vw-a text-decoration-none" href="#studtViewMdl" data-toggle="modal" data-id-attendances="<?php echo $sciPap->getIdAttendances(); ?>"><?php echo $sciPap->author; ?></a>
                             <?php
                             // if author had partakers in writting 
                             if (count($DBC->selectPartakings($sciPap->getIdScientificPapers()))) {
                             ?>
-                                <sup><a class="par-vw-a" href="#sciPapPrtViewMdl" data-toggle="modal" data-id-scientific-papers="<?php echo $sciPap->getIdScientificPapers(); ?>">Soavtorji</a></sup>
+                                <sup><a class="par-vw-a text-decoration-none" href="#sciPapPrtViewMdl" data-toggle="modal" data-id-scientific-papers="<?php echo $sciPap->getIdScientificPapers(); ?>">Soavtorji</a></sup>
                             <?php
                             }
                             ?>
@@ -67,14 +71,18 @@ include_once './nav.php';
                         <td><?php echo $sciPap->getType(); ?></td>
                         <td><?php echo (new DateTime($sciPap->getWritten()))->format('d-m-Y'); ?></td>
                         <td>
-                            <a class="doc-vw-a" href="#sciPapDocsViewMdl" data-toggle="modal" data-id-scientific-papers="<?php echo $sciPap->getIdScientificPapers(); ?>">Pregled</a>
+                            <a href="#sciPapDocsViewMdl" data-toggle="modal">
+                            <img src="/eArchive/custom/img/previewSciPapers.png" alt="Pregled"  class="doc-vw-img" data-id-scientific-papers="<?php echo $sciPap->getIdScientificPapers(); ?>" data-toggle="tooltip" title="Pregled">
+                        </a>
                         </td>
                         <td>
                             <?php
                             // if graduated on the scientific paper
                             if ($sciPap->id_certificates != NULL) {
                             ?>
-                                <a class="cert-vw-a" href="#gradCertViewMdl" data-toggle="modal" data-id-attendances="<?php echo $sciPap->getIdAttendances(); ?>">Pregled</a>
+                                <a class="cert-vw-a" href="#gradCertViewMdl" data-toggle="modal">
+                            <img src="/eArchive/custom/img/previewCertificate.png" alt="Pregled" class="cert-vw-img" data-id-attendances="<?php echo $sciPap->getIdAttendances(); ?>" data-toggle="tooltip" title="Pregled">
+                            </a>
                             <?php
                             } // if
                             ?>
@@ -87,8 +95,10 @@ include_once './nav.php';
         </table>
     </div>
 </div>
-<script src="./custom/js/index.js"></script>
+
+<!-- Add custom stylesheet --> 
 <link rel="stylesheet" href="./custom/css/index.css"> 
+
 <?php
 
 // script import declaration
