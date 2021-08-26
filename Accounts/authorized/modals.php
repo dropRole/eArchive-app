be0d07f6969ed14ea91bb7f70aa3db1323dedb38
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-26 19:46:57 +0200

    Update modals.php
    Altering elements identifiers.

--- Accounts/authorized/modals.php
+++ Accounts/authorized/modals.php
@@ -14,28 +14,28 @@
                     <div id="studentBasics" class="row px-lg-3 px-sm-1">
                         <div class="form-group col-lg-6 col-12">
                             <label class="w-100">Ime
-                                <input id="nameInptEl" class="form-control" type="text" name="name" required>
+                                <input id="name" class="form-control" type="text" name="name" required>
                             </label>
                         </div>
                         <div class="form-group col-lg-6 col-12">
                             <label class="w-100">Priimek
-                                <input id="surnameInptEl" class="form-control" type="text" name="surname" required>
+                                <input id="surname" class="form-control" type="text" name="surname" required>
                             </label>
                         </div>
                         <div class="form-group col-6">
                             <label class="w-100">E-naslov
-                                <input id="emailInptEl" class="form-control" type="email" name="email">
+                                <input id="email" class="form-control" type="email" name="email">
                             </label>
                         </div>
                         <div class="form-group col-6">
                             <label class="w-100">Telefon
-                                <input id="telInptEl" class="form-control" type="text" name="telephone">
+                                <input id="telephone" class="form-control" type="text" name="telephone">
                             </label>
                         </div>
                         <p class="h6 col-12">Rojen</p>
                         <div class="form-group col-lg-6 col-12">
                             <label class="w-100">Država
-                                <select id="birthCtrySelEl" class="form-control">
+                                <select id="birthCountry" class="form-control">
                                     <?php
                                     foreach ($DBC->selectCountries() as $country) {
                                     ?>
@@ -48,7 +48,7 @@
                         </div>
                         <div class="form-group col-lg-6 col-12">
                             <label class="w-100">Kraj
-                                <select id="birthPostCodeSelEl" class="form-control" name="id_postal_codes" required>
+                                <select id="birthPostCode" class="form-control" name="id_postal_codes" required>
                                     <?php
                                     foreach ($DBC->selectCountries()[0]->getIdCountries() as $postalCode) {
                                     ?>
@@ -67,7 +67,7 @@
                             <input type="hidden" name="residences[0][status]" value="STALNO">
                             <div class="form-group col-lg-4 col-12">
                                 <label class="w-100">Država
-                                    <select id="permResCtrySelEl" class="form-control" data-target="permResPostCodeSelEl">
+                                    <select id="permResCtry" class="form-control" data-target="permResPostCode">
                                         <?php
                                         foreach ($DBC->selectCountries() as $id_countries) {
                                         ?>
@@ -80,7 +80,7 @@
                             </div>
                             <div class="form-group col-lg-4 col-12">
                                 <label class="w-100">Kraj
-                                    <select id="permResPostCodeSelEl" class="form-control" name="residences[0][id_postal_codes]" required>
+                                    <select id="permResPostCode" class="form-control" name="residences[0][id_postal_codes]" required>
                                         <?php
                                         foreach ($DBC->selectPostalCodes($DBC->selectCountries()[0]->getIdCountries()) as $postalCode) {
                                         ?>
@@ -93,13 +93,13 @@
                             </div>
                             <div class="form-group col-lg-4 col-12">
                                 <label class="w-100">Naslov
-                                    <input id="permResAddressInptEl" class="form-control" type="text" name="residences[0][address]" required>
+                                    <input id="permResAddr" class="form-control" type="text" name="residences[0][address]" required>
                                 </label>
                             </div>
                         </div>
                         <p class="h6"><strong>Začasna bivališča</strong></p>
                         <div class="d-flex justify-content-center col-12">
-                            <img id="addTempResBtn" src="/eArchive/custom/img/add.png" alt="Dodaj bivališče" data-toggle="tooltip" title="Dodaj">
+                            <img id="addTempRes" src="/eArchive/custom/img/add.png" alt="Dodaj bivališče" data-toggle="tooltip" title="Dodaj">
                         </div>
                     </div>
                     <div id="attendances" class="px-lg-3 px-sm-2 pb-3">
@@ -109,7 +109,7 @@
                             <div class="form-group col-lg-6 col-12">
                                 <label class="w-100">
                                     Fakulteta
-                                    <select id="facSelEl" class="form-control" name="attendances[0][id_faculties]" required>
+                                    <select id="faculty" class="form-control" name="attendances[0][id_faculties]" required>
                                         <?php
                                         foreach ($faculties = $DBC->selectFaculties() as $faculty) {
                                         ?>
@@ -122,7 +122,7 @@
                             </div>
                             <div class="form-group col-lg-6 col-12">
                                 <label class="w-100">Program(polje, stopnja, trajanje)
-                                    <select id="progSelEl" class="form-control" name="attendances[0][id_programs]" required>
+                                    <select id="program" class="form-control" name="attendances[0][id_programs]" required>
                                         <?php
                                         foreach ($DBC->selectPrograms($DBC->selectFaculties()[0]->getIdFaculties()) as $program) {
                                         ?>
@@ -135,23 +135,23 @@
                             </div>
                             <div class="form-group col-lg-4 col-6">
                                 <label class="w-100">Vpisan
-                                    <input id="enrlInptEl" class="form-control" type="date" name="attendances[0][enrolled]" required>
+                                    <input id="enrolled" class="form-control" type="date" name="attendances[0][enrolled]" required>
                                 </label>
                             </div>
                             <div class="form-group col-lg-4 col-6">
                                 <label class="w-100">Indeks
-                                    <input id="indexInptEl" class="form-control" type="text" name="attendances[0][index]" required>
+                                    <input id="index" class="form-control" type="text" name="attendances[0][index]" required>
                                 </label>
                             </div>
                             <div class="d-flex align-items-center justify-content-center form-group col-lg-4 col-12">
                                 <label class="mt-2">
-                                    <input id="gradCheckBox" class="mr-2" type="checkbox" data-index="0" data-lbl-num="0">
+                                    <input id="graduation" class="mr-2" type="checkbox" data-index="0" data-lbl-num="0">
                                     Diplomiral
                                 </label>
                             </div>
                         </div>
                         <div class="d-flex justify-content-center col-12">
-                            <img id="addAttendanceBtn" src="/eArchive/custom/img/add.png" alt="Dodaj študij" data-toggle="tooltip" title="Dodaj">
+                            <img id="addAttendance" src="/eArchive/custom/img/add.png" alt="Dodaj študij" data-toggle="tooltip" title="Dodaj">
                         </div>
                     </div>
                     <input class="btn btn-warning offset-lg-5 offset-3 col-lg-2 col-6 my-2" type="submit" value="Vstavi">
@@ -182,12 +182,12 @@
                     <div id="particulars" class="row">
                         <div class="form-group col-12">
                             <label class="w-100">Predmet
-                                <input id="topicInptEl" class="form-control" type="text" name="topic" required>
+                                <input id="topic" class="form-control" type="text" name="topic" required>
                             </label>
                         </div>
                         <div class="form-group col-6">
                             <label class="w-100">Vrsta
-                                <select id="typeInptEl" class="form-control" name="type">
+                                <select id="type" class="form-control" name="type">
                                     <option value="DOKTORSKO DELO">Doktorsko delo</option>
                                     <option value="MAGISTRSKO DELO">Magistrsko delo</option>
                                     <option value="DIPLOMSKO DELO">Diplomsko delo</option>
@@ -198,17 +198,17 @@
                         </div>
                         <div class="form-group col-6">
                             <label class="w-100">Napisano
-                                <input id="writtenInptEl" class="form-control" type="date" name="written" required>
+                                <input id="written" class="form-control" type="date" name="written" required>
                             </label>
                         </div>
                     </div>
                     <div class="row">   
-                        <div id="sciPapPartakers" class="col-lg-6 col-12 mb-3">
+                        <div id="partakers" class="col-lg-6 col-12 mb-3">
                             <p class="h6"><strong>Soavtorji</strong></p>
                             <div class="d-flex justify-content-center col-12">
-                                <button id="addPartakerBtn" type="button" data-toggle="tooltip" title="Dodeli"></button>
+                                <button id="addPartaker" type="button" data-toggle="tooltip" title="Dodeli"></button>
                             </div>
-                            <datalist id="students">
+                            <datalist id="studentDatalist">
                                 <?php
                                 // denote student as potential partaker on a scientific paper
                                 foreach ($DBC->selectStudents() as $student) {
@@ -219,30 +219,30 @@
                                 ?>
                             </datalist>
                         </div>
-                        <div id="sciPapMentors" class="col-lg-6 col-12 mb-3">
+                        <div id="mentors" class="col-lg-6 col-12 mb-3">
                             <p class="h6"><strong>Mentorji</strong></p>
                             <div class="d-flex justify-content-center col-12">
-                                <button id="addMentorBtn" type="button" data-toggle="tooltip" title="Dodeli"></button>
+                                <button id="addMentor" type="button" data-toggle="tooltip" title="Dodeli"></button>
                             </div>
                         </div>
                     </div>
-                    <div id="sciPapDocs" class="mb-3">
+                    <div id="documents" class="mb-3">
                         <p class="h6"><strong>Dokumentacija</strong></p>
                         <div class="row">
                             <div class="form-group col-6">
                                 <label class="w-100">Verzija
-                                    <input id="versionInptEl" class="form-control" type="text" name="documents[0][version]" required>
+                                    <input id="version" class="form-control" type="text" name="documents[0][version]" required>
                                 </label>
                             </div>
                             <div class="form-group col-6">
-                                <input id="docNameInptEl" type="hidden" name="documents[0][name]" value="">
+                                <input id="document" type="hidden" name="documents[0][name]" value="">
                                 <label class="w-100 file-label">Dokument
-                                    <input id="docInptEl" type="file" name="document[]" accept=".pdf" required>
+                                    <input id="file" type="file" name="document[]" accept=".pdf" required>
                                 </label>
                             </div>
                         </div>
                         <div class="d-flex justify-content-center col-12">
-                            <button id="addDocBtn" type="button" data-toggle="tooltip" title="Dodaj"></button>
+                            <button id="addDocument" type="button" data-toggle="tooltip" title="Dodaj"></button>
                         </div>
                     </div>
                     <input class="btn btn-warning offset-lg-5 offset-3 col-lg-2 col-6" type="submit" value="Dodaj">
@@ -252,13 +252,13 @@
     </div>
 </div>
 <!-- Modal for viewing graduation certificate obtained attending the program  -->
-<div id="gradCertSelMdl" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div id="certSelMdl" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content"></div>
     </div>
 </div>
 <!-- Modal for uploading graduation certificate and inserting data regarding its issuance and defence dates  -->
-<div class="modal fade" id="gradCertUplMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModlaLabel" aria-hidden="true">
+<div class="modal fade" id="certUplMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModlaLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -268,23 +268,23 @@
                 </button>
             </div>
             <div class="modal-body">
-                <form id="gradCertUplFrm">
+                <form id="certUplFrm">
                     <input type="hidden" name="id_attendances" value="">
                     <div class="row">
                         <div class="form-group col-12">
                             <input type="hidden" name="certificate" value="">
                             <label class="w-100 file-label">Certifikat
-                                <input id="certInptEl" type="file" name="certificate[]" required>
+                                <input id="certificate" type="file" name="certificate[]" required>
                             </label>
                         </div>
                         <div class="form-group col-6">
                             <label class="w-100">Zagovarjan
-                                <input id="defendedInptEl" class="form-control" type="date" name="defended" required>
+                                <input id="defended" class="form-control" type="date" name="defended" required>
                             </label>
                         </div>
                         <div class="form-group col-6">
                             <label class="w-100">Izdan
-                                <input id="issuedInptEl" class="form-control" type="date" name="issued" required>
+                                <input id="issued" class="form-control" type="date" name="issued" required>
                             </label>
                         </div>
                     </div>
@@ -310,7 +310,7 @@
                     <input type="hidden" name="index">
                     <div class="form-group">
                         <label class="w-100">Geslo
-                            <input id="passInptEl" class="form-control" name="pass" required>
+                            <input id="password" class="form-control" name="pass" required>
                         </label>
                     </div>
                     <input class="btn btn-warning offset-lg-5 offset-3 col-lg-2 col-6" type="submit" value="Ustvari">
@@ -320,7 +320,7 @@
     </div>
 </div>
 <!-- Modal for reporting on performed operations -->
-<div class="modal fade" id="repMdl" tabindex="-1" role="dialog" aria-labelledby="reportMdl" aria-hidden="true">
+<div class="modal fade" id="reportModal" tabindex="-1" role="dialog" aria-labelledby="reportMdl" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
