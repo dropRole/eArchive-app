82ef79077ac1928eab841e6d4a903d5b7724adea
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-28 19:41:38 +0200

    Update modals.php
    Disposing of and altering id global attribute values of elements.

--- Accounts/student/modals.php
+++ Accounts/student/modals.php
@@ -1,5 +1,5 @@
 <!-- Modal for inserting data concerning scientific paper and uploading its documents -->
-<div class="modal fade" id="sciPapInsrMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="sciPapInsMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -9,17 +9,17 @@
                     </button>
             </div>
             <div class="modal-body">
-                <form id="sciPapInsrFrm">
+                <form id="sciPapInsFrm">
                     <input type="hidden" name="id_attendances" value="<?php echo $DBC->selectStudentsByIndex($_SESSION['index'])[0]->id_attendances; ?>">
                     <div id="particulars" class="row">
                         <div class="form-group col-12">
                             <label class="w-100">Predmet
-                                <input id="topicInptEl" class="form-control" type="text" name="topic" required>
+                                <input class="form-control" type="text" name="topic" required>
                             </label>
                         </div>
                         <div class="form-group col-6">
                             <label class="w-100">Vrsta
-                                <select id="typeInptEl" class="form-control" name="type">
+                                <select class="form-control" name="type">
                                     <option value="DOKTORSKO DELO">Doktorsko delo</option>
                                     <option value="MAGISTRSKO DELO">Magistrsko delo</option>
                                     <option value="DIPLOMSKO DELO">Diplomsko delo</option>
@@ -30,17 +30,17 @@
                         </div>
                         <div class="form-group col-6">
                             <label class="w-100">Napisano
-                                <input id="writtenInptEl" class="form-control" type="date" name="written" required>
+                                <input class="form-control" type="date" name="written" required>
                             </label>
                         </div>
                     </div>
                     <div class="row">   
-                        <div id="sciPapPartakers" class="col-lg-6 col-12 mb-3">
+                        <div id="partakers" class="col-lg-6 col-12 mb-3">
                             <p class="h6"><strong>Soavtorji</strong></p>
                             <div class="d-flex justify-content-center col-12">
-                                <button id="addPartakerBtn" type="button" data-toggle="tooltip" title="Dodeli"></button>
+                                <button id="addPartaker" type="button" data-toggle="tooltip" title="Dodeli"></button>
                             </div>
-                            <datalist id="students">
+                            <datalist id="studentDatalist">
                                 <?php
                                 // denote student as potential partaker on a scientific paper
                                 foreach ($DBC->selectStudents() as $student) {
@@ -51,30 +51,30 @@
                                 ?>
                             </datalist>
                         </div>
-                        <div id="sciPapMentors" class="col-lg-6 col-12 mb-3">
+                        <div id="mentors" class="col-lg-6 col-12 mb-3">
                             <p class="h6"><strong>Mentorji</strong></p>
                             <div class="d-flex justify-content-center col-12">
-                                <button id="addMentorBtn" type="button" data-toggle="tooltip" title="Dodeli"></button>
+                                <button id="addMentor" type="button" data-toggle="tooltip" title="Dodeli"></button>
                             </div>
                         </div>
                     </div>
-                    <div id="sciPapDocs" class="mb-3">
+                    <div id="documents" class="mb-3">
                         <p class="h6"><strong>Dokumentacija</strong></p>
                         <div class="row">
                             <div class="form-group col-6">
                                 <label class="w-100">Verzija
-                                    <input id="versionInptEl" class="form-control" type="text" name="documents[0][version]" required>
+                                    <input class="form-control" type="text" name="documents[0][version]" required>
                                 </label>
                             </div>
                             <div class="form-group col-6">
-                                <input id="docNameInptEl" type="hidden" name="documents[0][name]" value="">
+                                <input type="hidden" name="documents[0][name]" value="">
                                 <label class="w-100 file-label">Dokument
-                                    <input id="docInptEl" type="file" name="document[]" accept=".pdf" required>
+                                    <input type="file" name="document[]" accept=".pdf" required>
                                 </label>
                             </div>
                         </div>
                         <div class="d-flex justify-content-center col-12">
-                            <button id="addDocBtn" type="button" data-toggle="tooltip" title="Dodaj"></button>
+                            <button id="addDocument" type="button" data-toggle="tooltip" title="Dodaj"></button>
                         </div>
                     </div>
                     <input class="btn btn-warning offset-lg-5 offset-3 col-lg-2 col-6" type="submit" value="Dodaj">
@@ -84,13 +84,13 @@
     </div>
 </div>
 <!-- Modal for uploading account avatar -->
-<div class="modal fade" id="acctAvtrUpldMdl" tabindex="-1" role="dialog" aria-labelledby="reportMdl" aria-hidden="true">
+<div class="modal fade" id="acctInsMdl" tabindex="-1" role="dialog" aria-labelledby="reportMdl" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div id="currAvtr">
+            <div id="avatar">
                 <img src="<?php echo $DBC->hasAcctAvatar($_SESSION['index']) ? "/eArchive/{$DBC->hasAcctAvatar($_SESSION['index'])}"  : '/eArchive/custom/img/defaultAvatar.png'; ?>">
             </div>
-            <form id="acctAvtrUpldFrm">
+            <form id="avtrUplFrm">
                 <div class="form-group d-flex flex-column align-items-center">
                     <input type="hidden" name="id_attendances" value="<?php echo $DBC->selectStudentsByIndex($_SESSION['index'])[0]->id_attendances; ?>">
                     <label class="file-label w-50">Avatar
@@ -104,7 +104,7 @@
     </div>
 </div>
 <!-- Modal for reporting on performed operations -->
-<div class="modal fade" id="rprtMdl" tabindex="-1" role="dialog" aria-labelledby="reportMdl" aria-hidden="true">
+<div class="modal fade" id="reportModal" tabindex="-1" role="dialog" aria-labelledby="reportMdl" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -117,5 +117,3 @@
         </div>
     </div>
 </div>
-<!-- Custom stylesheet for modals -->
-<link rel="stylesheet" href="/eArchive/custom/css/modals.css">
