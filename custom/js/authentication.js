00e2e2f3b9225f80cf909b03fc8aa187be260de3
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-31 10:24:48 +0200

    Optimizing the code

--- custom/js/authentication.js
+++ custom/js/authentication.js
@@ -1,46 +1,46 @@
 // IIFE
 (() => {
     // global variable declaration
-    var lgnFrm = document.getElementById('lgnFrm') // login form
+    var loginForm = document.getElementById('loginForm') // login form
         /*
-         *   instantiate an object of integrated XHR interface and make an asynchronous operation on a script   
+         *   instantiate an XHR interface object an asynchronously perform the script   
          *   @param String script
          *   @param String method
          *   @param String responseType 
-         *   @param FormData frmData 
+         *   @param FormData formData 
          */
-    let request = (script, method, resType = '', frmData = null) => {
+    let request = (script, method, responseType = '', formData = null) => {
             return new Promise((resolve, reject) => {
                 // instanitate an XHR object
                 let xmlhttp = new XMLHttpRequest()
                 xmlhttp.addEventListener(
                         'load',
                         () => {
-                            // resolve the promise if transaction was successful
+                            // if transaction was successful
                             resolve(xmlhttp.response)
                         }
                     ) // addEventListener
                 xmlhttp.addEventListener(
                         'error',
                         () => {
-                            // reject the promise if transaction encountered an error
-                            reject('Prišlo je do napake na strežniku!')
+                            // if transaction encountered an error
+                            reject('Prišlo je do napake na strežniku pri obdelavi zahteve!')
                         }
                     ) // addEventListener
                 xmlhttp.open(method, script, true)
-                xmlhttp.responseType = resType
-                xmlhttp.send(frmData)
+                xmlhttp.responseType = responseType
+                xmlhttp.send(formData)
             })
         } // request
 
-    let checkAcctCred = e => {
+    let checkAccountCredentials = e => {
             // prevent default action of form submit
             e.preventDefault();
             request(
                     '/eArchive/Accounts/authentication.php',
                     'POST',
                     'json',
-                    (new FormData(lgnFrm))
+                    (new FormData(loginForm))
                 )
                 .then(response => {
                     let observer = new MutationObserver(() => {
@@ -50,21 +50,22 @@
                                     this.location.href = 'index.php'
                                 }, 2000) // setTimeout
                         }),
-                        rprt = document.createElement('p')
+                        report = document.createElement('p')
                     observer.observe(
-                        document.getElementById('lgnRprt'), {
+                        document.getElementById('loginReport'), {
                             attributes: false,
                             childList: true,
                             subtree: false
                         }
                     )
-                    rprt.classList = 'text-info font-italic'
-                    rprt.textContent = response.mssg
-                    document.getElementById('lgnRprt').innerHTML = ''
-                    document.getElementById('lgnRprt').appendChild(rprt)
+                    report.classList = 'text-info font-italic'
+                    report.textContent = response.message
+                    document.getElementById('loginReport').innerHTML = ''
+                    document.getElementById('loginReport').appendChild(report)
                 })
                 .catch(error => alert(error))
-        } // checkAcctCred
-    lgnFrm.addEventListener('submit', checkAcctCred)
+        } // checkAccountCredentials
+
+    loginForm.addEventListener('submit', checkAccountCredentials)
         // check out account credentials and respond respectively
 })()
