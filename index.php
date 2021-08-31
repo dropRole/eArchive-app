acd3b602a22c84290c75385f5573028b8a312734
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-31 09:54:51 +0200

    Script content interpolation
    Interpolated modals.php script content and consequential exclusion and deletion of it.

--- index.php
+++ index.php
@@ -15,6 +15,9 @@ include_once './nav.php';
 
 ?>
 
+<!-- Add custom stylesheet --> 
+<link rel="stylesheet" href="./custom/css/index.css"> 
+
 <!-- Deferred load of JS script -->
 <script defer src="./custom/js/index.js"></script>
 
@@ -96,14 +99,78 @@ include_once './nav.php';
     </div>
 </div>
 
-<!-- Add custom stylesheet --> 
-<link rel="stylesheet" href="./custom/css/index.css"> 
+<!-- Modal for scientific paper partakers view -->
+<div class="modal fade" id="sciPapInsMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Soavtroji dela</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal for scientific paper document view -->
+<div class="modal fade" id="sciPapSelMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Dokumenta dela</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal for graduation certificate view -->
+<div class="modal fade" id="certSelMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal for student particulars view -->
+<div class="modal fade" id="stuSelMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+            
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal for scientific paper mentor view -->
+<div class="modal fade" id="mentSelMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+            
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 
 // script import declaration
 
-include_once './modals.php';
 include_once './footer.php';
 
 ?>
