6768507aa4a9093d14b4fe7d8113fc64e0bbc9c5
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-31 09:50:12 +0200

    Update modals.php
    Changing modal identifiers.

--- modals.php
+++ modals.php
@@ -1,5 +1,5 @@
 <!-- Modal for scientific paper partakers view -->
-<div class="modal fade" id="sciPapPrtViewMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="sciPapInsMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -15,7 +15,7 @@
     </div>
 </div>
 <!-- Modal for scientific paper document view -->
-<div class="modal fade" id="sciPapDocsViewMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="sciPapSelMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -31,7 +31,7 @@
     </div>
 </div>
 <!-- Modal for graduation certificate view -->
-<div class="modal fade" id="gradCertViewMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="certSelMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -46,7 +46,7 @@
     </div>
 </div>
 <!-- Modal for student particulars view -->
-<div class="modal fade" id="studtViewMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="stuSelMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-body">
@@ -56,7 +56,7 @@
     </div>
 </div>
 <!-- Modal for scientific paper mentor view -->
-<div class="modal fade" id="sciPapMenViewMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="mentSelMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-body">
