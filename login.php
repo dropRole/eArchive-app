38262ff7f3ff1ca12ff727f0810540c1105d7f16
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-31 10:25:59 +0200

    Update login.php
    Modified values of global ID attributes.

--- login.php
+++ login.php
@@ -8,31 +8,31 @@ include_once './nav.php';
 
 ?>
 
+<!-- Login form stylesheet -->
+<link href="/eArchive/custom/css/login.css" rel="stylesheet" />
+
 <!-- User authentication script -->
 <script defer src="/eArchive/custom/js/authentication.js"></script>
 
 <section>
     <div class="d-flex justify-content-center my-4">
-        <form id="lgnFrm" method="POST" action="./Accounts/authentication.php" class="p-3">
+        <form id="loginForm" method="POST" action="./Accounts/authentication.php" class="p-3">
             <p class="h2">Prijava</p>
             <div class="form-group">
                 <label>Indeks
-                    <input id="indexInptEl" class="form-control" type="text" name="index" autocomplete="on" required>
+                    <input class="form-control" type="text" name="index" autocomplete="on" required>
                 </label>
             </div>
             <div class="form-group">
                 <label>Geslo
-                    <input id="pаssInptEl" class="form-control" type="password" name="pass" autocomplete="current-password" required>
+                    <input class="form-control" type="password" name="pass" autocomplete="current-password" required>
                 </label>
             </div>
-            <div id="lgnRprt"></div>
-            <input id="lgnBtn" class="btn btn-dark" type="submit" value="Potrdi">
+            <div id="loginReport"></div>
+            <input id="loginButton" class="btn btn-dark" type="submit" value="Potrdi">
         </form>
     </div>
 </section>
-
-<!-- Login form stylesheet -->
-<link href="/eArchive/custom/css/login.css" rel="stylesheet" />
 
 <?php
 
