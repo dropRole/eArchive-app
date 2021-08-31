9b940d276c6f1c1467eface859ae83953b36dc3d
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-08-31 09:29:32 +0200

    Update autoload.php
    Requiring class scripts.

--- autoload.php
+++ autoload.php
@@ -2,11 +2,7 @@
 
 // register class autoloader
 spl_autoload_register(function ($class) {
-    // if it's extended PDO class
-    if ($class === 'DBC')
         require_once "{$class}.php";
-    else
-        include_once "{$class}.php";
 });
 
 ?>
