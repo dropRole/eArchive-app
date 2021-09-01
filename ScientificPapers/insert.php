e82ea3cac503df8a411749398a8c701c6a6e2ef3
Dušan Radosavljević <dusan.radosavljevic82@gmail.com>
2021-09-01 14:59:38 +0200

    Update insert.php
    Refactored locally encompassed $idScientificPapers variable.

--- ScientificPapers/insert.php
+++ ScientificPapers/insert.php
@@ -22,19 +22,19 @@ if (isset($id_attendances, $topic, $type, $written, $documents)) {
     // establish a new database connection
     $DBC = new DBC($_SESSION['user'], $_SESSION['pass']);
     // attempt an insert
-    $idScientificPapers = $DBC->insertScientificPaper($id_attendances, $topic, $type, (new DateTime($written)));
+    $id_scientific_papers = $DBC->insertScientificPaper($id_attendances, $topic, $type, (new DateTime($written)));
     // if an attempt was successful 
-    if ($idScientificPapers) {
+    if ($id_scientific_papers) {
         // if there were partakers in writting
         if (isset($_POST['partakers']))
             foreach ($_POST['partakers'] as $partaker)
-                $DBC->insertPartaker($report['id_scientific_papers'], $DBC->selectStudentsByIndex($partaker['index'])[0]->id_attendances, $DBC->selectStudentsByIndex($partaker['index'])[0]->fullname, $partaker['part']);
+                $DBC->insertPartaker($id_scientific_papers, $DBC->selectStudentsByIndex($partaker['index'])[0]->id_attendances, $DBC->selectStudentsByIndex($partaker['index'])[0]->fullname, $partaker['part']);
         // if scientific paper was mentored
         if (isset($_POST['mentors'])) {
             foreach ($_POST['mentors'] as $mentor)
-                $DBC->insertMentor($report['id_scientific_papers'], $mentor['id_faculties'], $mentor['mentor'], $mentor['taught'], $mentor['email'], $mentor['telephone']);
+                $DBC->insertMentor($id_scientific_papers, $mentor['id_faculties'], $mentor['mentor'], $mentor['taught'], $mentor['email'], $mentor['telephone']);
         } // if
         foreach ($documents as $document)
-            $DBC->uploadDocument($report['id_scientific_papers'], $document['version'], $document['name']);
+            $DBC->uploadDocument($id_scientific_papers, $document['version'], $document['name']);
     } // if
 } // if  
